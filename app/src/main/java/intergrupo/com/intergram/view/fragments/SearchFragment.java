1b5249a748b1040d9279b7140b80b9a198ad2c94
Jaime Alexander Uribe <jauribe@intergrupo.com>
2019-02-04 17:19:02 -0500

    asignando buscar

--- app/src/main/java/intergrupo/com/intergram/view/fragments/SearchFragment.java
+++ app/src/main/java/intergrupo/com/intergram/view/fragments/SearchFragment.java
@@ -3,11 +3,20 @@ package intergrupo.com.intergram.view.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import intergrupo.com.intergram.R;
+import intergrupo.com.intergram.adapter.PictureAdapterRecycleView;
+import intergrupo.com.intergram.model.Picture;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,8 +32,47 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_search, container, false);
+        //showToolbar("",false,view);
+
+        RecyclerView picturesRecycle=view.findViewById(R.id.searchRecycle);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(),2, GridLayoutManager.VERTICAL, false);
+        //gridLayoutManager.setOrientation(GridLayoutManager.VERTICAL);
+
+        picturesRecycle.setLayoutManager(gridLayoutManager);
+
+        PictureAdapterRecycleView pictureAdapterRecycleView = new PictureAdapterRecycleView(buildPictures(), R.layout.cardview_picture, getActivity());
+
+        picturesRecycle.setAdapter(pictureAdapterRecycleView);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return view;
+
+
+    }
+
+    public void showToolbar(String title, boolean upBotton, View view) {
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+
+        /*por trabajar con soporte...getActivity se castea a lo appComponenteActivity
+        y traeme un elemento igualito al del conteiner*/
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+
+        //en el caso que tenga botton de regreso
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upBotton);
+    }
+
+    public ArrayList<Picture> buildPictures() {
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("http://2.bp.blogspot.com/-uNFR4JIdoAU/UUK-CtzWUJI/AAAAAAAADzU/eLe6qFm_Y3w/s640/paisajes+02.jpg", "Jaime Uribe", "4 días", "3 me gusta"));
+        pictures.add(new Picture("http://eldiario.com.co/uploads/userfiles/20150103/image/P%C3%81G3-copia.jpg", "Tatiana Lopez", "1 días", "50"));
+        pictures.add(new Picture("https://cdn.logitravel.com/wsimgresize/resize/crop/450/330/cdn.logitravel.com/contenidos/fotos/CIRCUITOS/900_900/1003312.jpg", "Liliana Tamaño", "17 días", "21 me gusta"));
+        pictures.add(new Picture("https://www.france-voyage.com/visuals/photos/paisajes-litoral-costa-azul-6509_w600.jpg", "Juan Lopez", "10 días", "15 me gusta"));
+
+        return pictures;
     }
 
 }
